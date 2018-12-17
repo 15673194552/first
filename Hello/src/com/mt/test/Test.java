642a582f2c7825ca0737bf827c082b8fb95fb8af
15673194552 <45931399+15673194552@users.noreply.github.com>
2018-12-17 17:58:32 +0800

    Update Test.java

--- Hello/src/com/mt/test/Test.java
+++ Hello/src/com/mt/test/Test.java
@@ -2,7 +2,7 @@ package com.mt.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("00000111");
+		System.out.println("0111111");
 		System.out.println("1111");
 		System.out.println("2222");
 	}
